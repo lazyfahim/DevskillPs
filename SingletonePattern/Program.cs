87a77304756264c68a9f44785ea8254884b64aa6
lazyfahim <fahimabrar13@gmail.com>
2020-06-05 18:40:05 +0600

    singletone pattern added

---
+++ SingletonePattern/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SingleTonExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var _logger = ErrorLogger.CreateLogger();
+            _logger.Log();
+        }
+    }
+    class ErrorLogger
+    {
+        private static ErrorLogger _errorlogger;
+        private ErrorLogger()
+        {
+
+        }
+        public static ErrorLogger CreateLogger()
+        {
+            if(_errorlogger == null)
+                return new ErrorLogger();
+            else
+                return _errorlogger;
+        }
+        public void Log()
+        {
+            Console.WriteLine("Logging");
+        }
+    }
+}
