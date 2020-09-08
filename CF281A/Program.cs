0f1ebaed27182e3b7006b8966738a549bd28e153
fahim abrar <fahimabrar13@gmail.com>
2020-09-08 14:29:21 +0600

    cf 281 a done

---
+++ CF281A/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace CF281A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String s;
+#if !ONLINE_JUDGE
+            Console.SetIn(File.OpenText("file.txt"));
+            //Console.SetOut(File.CreateText("out.txt"));
+#endif
+            s = Console.ReadLine();
+            char[] str = s.ToCharArray();
+            if (str[0] >= 97 && str[0] <= 122)
+                str[0] =(char) (str[0]-32);
+            Console.WriteLine(str);
+        }
+    }
+}
