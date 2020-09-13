b9cb80e62ea55a0743edccdd499fe2ec04fb30d6
fahim abrar <fahimabrar13@gmail.com>
2020-09-13 15:32:35 +0600

    977 a solved

---
+++ CF977A/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace CF977A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String s;
+#if !ONLINE_JUDGE
+            Console.SetIn(File.OpenText("file.txt"));
+            //Console.SetOut(File.CreateText("out.txt"));
+#endif
+            s = Console.ReadLine();
+            long[] num = Array.ConvertAll(s.Split(' '),x => long.Parse(x));
+            long n = num[0];
+            for(int i=1;i<=num[1];i++)
+            {
+                if (n % 10 == 0)
+                    n /= 10;
+                else
+                    n--;
+            }
+            Console.WriteLine(n);
+        }
+    }
+}
